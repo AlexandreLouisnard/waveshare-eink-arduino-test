338d2355d41f159d14ddc39e472c1b2214cca91a
Alexandre Louisnard <alouisnard@gmail.com>
2020-01-10 19:33:56 +0100

    Initial commit, working

---
+++ src/main.h
@@ -0,0 +1,26 @@
+void helloWorld();
+void helloWorldForDummies();
+void helloFullScreenPartialMode();
+void helloArduino();
+void helloEpaper();
+void helloValue(double v, int digits);
+void deepSleepTest();
+void showBox(uint16_t x, uint16_t y, uint16_t w, uint16_t h, bool partial);
+void drawCornerTest();
+void showFont(const char name[], const GFXfont* f);
+void drawFont(const char name[], const GFXfont* f);
+void showPartialUpdate();
+void drawBitmaps();
+void drawBitmaps200x200();
+void drawBitmaps3c128x296();
+
+
+
+
+
+
+
+
+
+
+
